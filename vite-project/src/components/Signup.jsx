b67f2c3e6efd1bda9e8cfdb89149ce82f32d9f48
Rithick-Roshan <rithick1887@gmail.com>
2025-02-08 12:50:22 +0530

    react update

--- vite-project/src/components/Signup.jsx
+++ vite-project/src/components/Signup.jsx
@@ -1,27 +1,23 @@
-import {useState,useEffect} from "react";
+import {useState} from "react";
 
-function Login(){
+function Signup(){
    const [email,setEmail]=useState("");
    const [password,setPassword]=useState("");
-  
-   function fun(event){
-     setEmail(event.target.value);
-      console.log(email);
-   }
+
     return(
     <> 
     <div className="login_conatiner">
         <div className="login">
-            <h1 className="Login_header">Login</h1>
+            <h1 className="Login_header">Signup</h1>
             <form action="" className="login_form">
                 <div className="login_input">
-                    <input type="email" value={email}  onChange={fun} placeholder="Enter Your Email" />
+                    <input type="email" placeholder="Enter Your Email" />
                 </div>
                 <div class="login_input">
                     <input type="password" placeholder="Enter Your Pass" />
                 </div>
                 <div class="login_button">
-                    <button class="login_button" ><a href="www.google.com">Login</a></button>
+                    <button class="login_button" ><a href="www.google.com">Sigup</a></button>
                 </div>
                 <div class="login_par">
                     <p>OR</p>
@@ -39,4 +35,4 @@ function Login(){
     );
 }
 
-export default Login;
+export default Signup;
